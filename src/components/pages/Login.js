e32e0607ced9901c8d072be1684a5f96a2b562da
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2020-07-02 10:36:26 +0100

    initial login form added

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import useForm from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import "./Login.scss"
 
 function Login() {
-    // const { register, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, errors } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
 
     return (
-        <form>
-            <input type="text" placeholder="Login ID" name="login-id" />
-            <input type="password" placeholder="Password" name="password" />
-            <input type="submit" />
-        </form>
+        <div className="app-container">
+            <header className="page-title">
+                Log In
+        </header>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input className="field" type="text" placeholder="Login ID" name="login-id" ref={register} />
+                <input className="field" type="password" placeholder="Password" name="password" ref={register({ required: "Password is invalid", minLength: 8 })} />
+                <input className="btn" type="submit" />
+                {errors.password && <p>{errors.password.message}</p>}
+            </form>
+        </div>
 
         // <div className="App">
         //     <header className="App-header">
