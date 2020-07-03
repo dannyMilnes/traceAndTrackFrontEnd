26cb5aa775ba26ea496d21438b76a7499055da4a
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2020-07-03 12:06:17 +0100

    global form styles added

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import "./Login.scss"
 
 function Login() {
     const { register, handleSubmit, errors } = useForm();
@@ -16,18 +15,14 @@ function Login() {
         </header>
 
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input className="field" type="text" placeholder="Login ID" name="login-id" ref={register} />
-                <input className="field" type="password" placeholder="Password" name="password" ref={register({ required: "Password is invalid", minLength: 8 })} />
+                <label>Login ID</label>
+                <input className="field" type="text" placeholder="" name="login-id" ref={register} />
+                <label>Password</label>
+                <input className="field" type="password" placeholder="" name="password" ref={register({ required: "Password is invalid", minLength: 8 })} />
                 <input className="btn" type="submit" />
                 {errors.password && <p>{errors.password.message}</p>}
             </form>
         </div>
-
-        // <div className="App">
-        //     <header className="App-header">
-        //         Log In
-        //     </header>
-        // </div>
 
     );
 }
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Register() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                Register
+        <div className="app-container">
+            <header className="page-title">
+                Register Venue
             </header>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="form-fields">
+                    <div className="form-field form-field--50">
+                        <label for="venueName">Venue Name</label>
+                        <input id="venueName" type="text" placeholder="" name="venueName" />
+                    </div>
+                    <div className="form-field form-field--50">
+                        <label for="username">Username</label>
+                        <input id="username" type="text" placeholder="" name="username" />
+                    </div>
+                    <div className="form-field form-field--50">
+                        <label for="email">E-Mail</label>
+                        <input id="email" type="email" placeholder="" name="email" />
+                    </div>
+                    <div className="form-field form-field--50">
+                        <label for="confirmEmail">Confirm E-Mail</label>
+                        <input id="confirmEmail" type="email" placeholder="" name="confirmEmail" />
+                    </div>
+                    <div className="form-field form-field--50">
+                        <label for="password">Password</label>
+                        <input id="password" type="password" placeholder="" name="password" />
+                    </div>
+                    <div className="form-field form-field--50">
+                        <label for="confirmPassword">Confirm Password</label>
+                        <input id="confirmPassword" type="password" placeholder="" name="confirmPassword" />
+                    </div>
+                </div>
+
+                <div className="form-fields">
+                    <div className="form-field form-field--50">
+                        <label for="venueName">Venue Name</label>
+                        <input id="venueName" type="text" placeholder="" name="venueName" />
+                    </div>
+                </div>
+
+                <div className="form__submit">
+                    <input className="btn" type="submit" value="Submit" />
+                </div>
+            </form>
         </div>
 
     );
